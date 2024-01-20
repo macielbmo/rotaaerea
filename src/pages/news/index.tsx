a8b5fe5b9cf285afdb5fc9834a11b351b42115d5
Maciel <macielbmo@gmail.com>
2024-01-20 10:05:13 -0300

    Correção para deploy

--- src/pages/news/index.tsx
+++ src/pages/news/index.tsx
@@ -80,7 +80,7 @@ export default function News() {
   useEffect(() => {
     async function fetchData() {
       try {
-        const apiUrl = `http://localhost:3001`;
+        const apiUrl = `https://rotaaerea-backend.vercel.app`;
 
         const maxResult = 10;
         const newsId = newsData?.id;
